18d453237aec7cc21db0fc88d8a3f8209f33743d
ShivamVerma12 <115446853+ShivamVerma12@users.noreply.github.com>
2023-07-06 14:41:33 +0530

    Update experiment.py

--- modules/experiment/experiment.py
+++ modules/experiment/experiment.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 from helper.helper import browser
-from azure.storage.blob import BlobServiceClient
+# from azure.storage.blob import BlobServiceClient
 
 global slug
 
