792ea67552c63671f4dd71de90b930e156781173
ShivamVerma12 <115446853+ShivamVerma12@users.noreply.github.com>
2024-01-16 18:05:10 +0530

    Update api_auth.groovy

--- api_auth.groovy
+++ api_auth.groovy
@@ -9,123 +9,26 @@ pipeline {
         WORKSPACE = "${params.WORKSPACE ?: 'API-AUT'}"
     }
     stages {
-        stage('Clear Workspace') {
-            steps {
-                deleteDir()
-            }
-        }
-        stage('Run Build CICD') {
-            when {
-                expression { params.WORKSPACE == 'API-AUT' }
-            }
-            steps {
-                script {
-                    // Trigger the 'CICD' job without parameters and wait for completion
-                    // def cicdJob = build(job: 'CICD', wait: true)
-
-                    // if (cicdJob.resultIsBetterOrEqualTo('SUCCESS')) {
-                    //     // Get the build number of the 'CICD' job
-                        def buildNumber = "20909"
-
-                        // Set the cicd_number parameter to pass it to the next stage
-                        cicd_number = "${buildNumber}"
-
-                    } else {
-                        error("Failed to build 'CICD' job")
-                    }
-                }
-            }
-        }
-        stage('Prepare Environment and Run API-AUT Tests') {
-            when {
-                expression { params.WORKSPACE == 'API-AUT' }
-            }
-            steps {
-                script {
-                    // Remove existing 'Collections' directory if it exists and run everything from that location
-                    sh 'rm -rf Collections'
-                    sh 'mkdir Collections'
-                    // Run terminal commands to prepare the environment
-                    sh '''
-                        cd Collections
-                        sudo apt install -y jq
-                        sudo npm install -g newman@4.5.6 --unsafe-perm=true --allow-root
-                        sudo npm install -g newman-reporter-junit --unsafe-perm=true --allow-root
-                        sudo npm install -g newman@4.5.6 postman-combine-collections --save --unsafe-perm=true --allow-root
-                        curl -X GET "https://api.getpostman.com/workspaces/b38ccd71-80f2-44f3-83ae-e81ccd48a930" -H "X-Api-Key: PMAK-6501d1a409dd38002bf3416c-07b1ed39641164457905361086d6e9a6ce" -H "Cache-Control: no-cache" -o API_AUT_workspace.json
-                    '''
-                    // Define the Python script within a Groovy triple-quoted string
-                    def api_aut_collections = """
+        stage('Create Token and upload report'){
+            steps{
+                script{
+                   def bearer = """
 import subprocess
-import json
-import time
+import requests
+response = requests.post('https://xray.cloud.getxray.app/api/v2/authenticate',
+                             data={'client_id': '5E4F5179DFC2445086DA4CAAC85A6D5D',
+                                   'client_secret': '71beabdad7d16921a226666b815d4200fb74e07d12bb04832d194a2edbf707eb'})
+parsed_auth = response.json()
+token = f'Bearer {parsed_auth}'
+curl_cmd = f"curl -H "Content-Type: test/xml" -X POST -H "Authorization:{token}" --data @resultsAB.xml  https://xray.cloud.getxray.app/api/v2/import/execution/junit?projectKey=KAN
 
-# Replace these values with your actual Postman API key and workspace ID
-postman_api_key = 'PMAK-6501d1a409dd38002bf3416c-07b1ed39641164457905361086d6e9a6ce'
-
-# Load your Postman workspace JSON file
-with open('API_AUT_workspace.json', 'r') as file:
-    workspace_data = json.load(file)
-
-# Extract collections from the nested structure
-collections = workspace_data['workspace']['collections']
-
-# Extract the environment from the nested structure (assuming there is only one environment)
-environment = workspace_data['workspace']['environments'][0]
-environment_uid = environment['uid']
-
-# Iterate through each collection in the workspace
-for collection in collections:
-    collection_id = collection['uid']
-    collection_name = collection['name']
-
-    # Construct the curl command
-    curl_command = f'curl -f --retry 5 -X GET "https://api.postman.com/collections/{collection_id}" -H "X-Api-Key: {postman_api_key}" -H "Cache-Control: no-cache" -o "{collection_name}.json"'
-
-    # Execute the curl command using subprocess
-    try {
-        subprocess.run(curl_command, shell=True, check=True)
-        print(f'Collection "{collection_name}" exported and saved as "{collection_name}.json"')
-
-        # Wait for a short duration to ensure the file is fully downloaded
-        time.sleep(1)
-
-        # Construct the jq command to edit the JSON file
-        jq_command = f'jq \'collection | del(.schema, .fork, .updatedAt, .uid)\' "{collection_name}.json" > "{collection_name}.api_aut.json"'
-
-        # Execute the jq command using subprocess
-        subprocess.run(jq_command, shell=True, check=True)
-        print(f'JSON file "{collection_name}.json" edited and saved as "{collection_name}.api_aut.json"')
-    } catch (subprocess.CalledProcessError e) {
-        print(f'Failed to export or edit collection "{collection_name}". Error: {e}')
-    }
-
-# Construct the curl command to export the environment
-curl_command_env = f'curl -X GET "https://api.postman.com/environments/{environment_uid}" -H "X-Api-Key: {postman_api_key}" -H "Cache-Control: no-cache" -o "api_aut_environment.json"'
-
-# Execute the curl command for the environment using subprocess
-try {
-    subprocess.run(curl_command_env, shell=True, check=True)
-    print(f'Environment exported successfully')
-} catch (subprocess.CalledProcessError e) {
-    print(f'Failed to export environment. Error: {e}')
-}
-"""
+                    """
                     // Write the Python script to a file
-                    writeFile(file: 'Collections/collections.py', text: api_aut_collections)
-                    sh '''
-                        cd Collections
-                        python3 collections.py
-                        postman-combine-collections --name API-AUT -f '*.api_aut.json' -o api_aut.collection.json
-                    '''
-                    // Run Newman tests and capture the output
-                    def newmanOutput = sh(script: '''
-                        cd Collections
-                        newman run api_aut.collection.json -r junit  --reporter-junit-export newman.xml -e api_aut_environment.json --insecure 
-                        exit 0  # Always exit with code 0
-                    ''', returnStatus: true)
+                    writeFile(file: 'collections.py', text: bearer)
                 }
             }
         }
+                
+        
     }
 }
